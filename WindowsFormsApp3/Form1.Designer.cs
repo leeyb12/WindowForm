ee680a2d507510b75269d32178847a4d9764f4e0
Yunbeom <dbsqja1233@naver.com>
2025-10-22 10:28:30 +0900

    lyb

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTodo = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.lstTodos = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(284, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ToDo 리스트";
+            // 
+            // txtTodo
+            // 
+            this.txtTodo.Font = new System.Drawing.Font("굴림", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtTodo.Location = new System.Drawing.Point(12, 112);
+            this.txtTodo.Name = "txtTodo";
+            this.txtTodo.Size = new System.Drawing.Size(532, 28);
+            this.txtTodo.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("굴림", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnAdd.Location = new System.Drawing.Point(603, 112);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(113, 28);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "추가";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Font = new System.Drawing.Font("굴림", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnEdit.Location = new System.Drawing.Point(603, 175);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(113, 28);
+            this.btnEdit.TabIndex = 3;
+            this.btnEdit.Text = "수정";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("굴림", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnDelete.Location = new System.Drawing.Point(603, 242);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(113, 28);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "삭제";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("굴림", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnSave.Location = new System.Drawing.Point(603, 310);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(113, 28);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "저장";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Font = new System.Drawing.Font("굴림", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnLoad.Location = new System.Drawing.Point(603, 378);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(113, 28);
+            this.btnLoad.TabIndex = 7;
+            this.btnLoad.Text = "불러오기";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // lstTodos
+            // 
+            this.lstTodos.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lstTodos.HideSelection = false;
+            this.lstTodos.Location = new System.Drawing.Point(12, 175);
+            this.lstTodos.Name = "lstTodos";
+            this.lstTodos.Size = new System.Drawing.Size(532, 209);
+            this.lstTodos.TabIndex = 6;
+            this.lstTodos.UseCompatibleStateImageBehavior = false;
+            this.lstTodos.SelectedIndexChanged += new System.EventHandler(this.LstTodos_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 450);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.lstTodos);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtTodo);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTodo;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.ListView lstTodos;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.IO;
+using System.Security.AccessControl;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        private readonly string filePath = "todos.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lstTodos.View = View.Details;
+            lstTodos.CheckBoxes = true;
+            lstTodos.FullRowSelect = true;
+            lstTodos.GridLines = true;
+
+            lstTodos.Columns.Clear();
+            lstTodos.Columns.Add("할 일", 400);
+
+            lstTodos.ItemChecked += LstTodos_ItemChecked;
+            lstTodos.SelectedIndexChanged += LstTodos_SelectedIndexChanged;
+        }
+
+        private void LstTodos_ItemChecked(object sender, ItemCheckedEventArgs e)
+        {
+            var item = e.Item;
+
+            if (item.Checked)
+            {
+                item.ForeColor = Color.Gray;
+                item.Font = new Font(item.Font, FontStyle.Strikeout);
+            }
+            else
+            {
+                item.ForeColor = Color.Black;
+                item.Font = new Font(item.Font, FontStyle.Regular);
+            }
+
+            lstTodos.Invalidate();
+            lstTodos.Update();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string text = txtTodo.Text.ToString();
+            if (string.IsNullOrWhiteSpace(txtTodo.Text))
+            {
+                MessageBox.Show("할 일을 입력해주세요!");
+                return;
+            }
+
+            ListViewItem item = new ListViewItem(text);
+            item.Checked = false;
+            lstTodos.Items.Add(item);
+            txtTodo.Clear();
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (lstTodos.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("수정할 항목을 선택하세요!");
+                return;
+            }
+
+            string newText = txtTodo.Text.Trim();
+            if (string.IsNullOrWhiteSpace(newText))
+            {
+                MessageBox.Show("수정할 내용을 선택하세요!");
+                return;
+            }
+
+            lstTodos.SelectedItems[0].Text = newText;
+            txtTodo.Clear();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (lstTodos.CheckedItems.Count > 0)
+            {
+                for (int i = lstTodos.CheckedItems.Count - 1; i >= 0; i--)
+                    lstTodos.Items.Remove(lstTodos.CheckedItems[i]);
+             }
+             else if (lstTodos.SelectedItems.Count > 0)
+             {
+                lstTodos.Items.Remove(lstTodos.SelectedItems[0]);
+             }
+             else
+             {
+                MessageBox.Show("삭제할 항목을 선택하거나 체크하세요.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+             }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            List<string> lines = new List<string>();
+            foreach (ListViewItem item in lstTodos.Items)
+            {
+                lines.Add($"{item.Checked} | {item.Text}");
+            }
+
+            File.WriteAllLines(filePath, lines, Encoding.UTF8);
+            MessageBox.Show("저장 완료!");
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            LoadTasks();
+        }
+        private void LoadTasks()
+        {
+            if (!File.Exists(filePath))
+            {
+                MessageBox.Show("저장된 파일이 없습니다.");
+                return;
+            }
+
+            try
+            {
+                lstTodos.Items.Clear();
+                var lines = File.ReadAllLines(filePath, Encoding.UTF8);
+
+                foreach (var line in lines)
+                {
+                    var parts = line.Split('|');
+                    if (parts.Length == 2)
+                    {
+                        bool isChecked= bool.TryParse(parts[0].Trim(), out bool result) && result;
+                        string text = parts[1].Trim();
+
+                        ListViewItem item = new ListViewItem(text);
+                        item.Checked = isChecked;
+
+                        if (isChecked)
+                        {
+                            item.ForeColor = Color.Gray;
+                            item.Font = new Font(lstTodos.Font, FontStyle.Strikeout);
+                        }
+                        
+                        lstTodos.Items.Add(item);
+
+                    }
+                }
+
+                MessageBox.Show("불러오기 완료!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("불러오기 중 오류:" + ex.Message);
+            }
+        }
+        private void LstTodos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           if (lstTodos.SelectedItems.Count > 0)
+            {
+                txtTodo.Text = lstTodos.SelectedItems[0].Text;
+            }
+        }
+    }
+}
